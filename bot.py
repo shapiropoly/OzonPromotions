34f9035790e2da72d744beaaff60648ab58d0240
SoniaFilozop <72881848+SoniaFilozop@users.noreply.github.com>
2024-05-13 12:34:57 +0300

    delete personal_account.py change bot.py, connect_store.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ import logging
 from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
 from config_reader import config
-from handlers import common, personal_account, connect_store
+from handlers import common, connect_store
 
 
 async def main():
@@ -15,7 +15,7 @@ async def main():
     dp = Dispatcher(storage=MemoryStorage())
     bot = Bot(config.bot_token.get_secret_value())
 
-    dp.include_routers(common.router, personal_account.router, connect_store.router)
+    dp.include_routers(common.router, connect_store.router)
 
     await dp.start_polling(bot)
 
--- handlers/connect_store.py
+++ handlers/connect_store.py
@@ -3,9 +3,9 @@ from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import StatesGroup, State
 from aiogram.types import Message, ReplyKeyboardRemove
 
-from texts.message import check_connect
-from texts.button import b_account
-from handlers.personal_account import Person
+from handlers.common import Common
+from texts.message import check_connect, account
+from texts.button import b_account, b_hello
 from keyboard.inline_keyboard import make_keyboard
 
 
@@ -13,12 +13,50 @@ router = Router()
 
 
 class Store(StatesGroup):
+    writing_name = State()
+    writing_client_id = State()
+    writing_api_key = State()
     choosing_store = State()
     choosing_settings = State()
 
 
+@router.callback_query(Common.choosing_moves, F.data == (b_hello[0]))
+async def name(callback: types.CallbackQuery, state: FSMContext):
+    await callback.message.answer(
+        text=account[0],
+        reply_markup=ReplyKeyboardRemove()
+    )
+    # TODO Сделать добавление имени в БД
+    await state.set_state(Store.writing_name)
+
+
+@router.message(Store.writing_name)
+async def surname(message: Message, state: FSMContext):
+    await message.answer(
+        text=account[2],
+        reply_markup=ReplyKeyboardRemove()
+    )
+    await message.answer(
+        text=account[3],
+        reply_markup=ReplyKeyboardRemove()
+    )
+    # TODO Сделать добавление client-id в БД
+    await state.set_state(Store.writing_client_id)
+
+
+@router.message(Store.writing_client_id)
+async def surname(message: Message, state: FSMContext):
+    await message.answer(
+        text=account[4],
+        reply_markup=ReplyKeyboardRemove()
+    )
+    # TODO Сделать добавление api-key в БД
+
+    await state.set_state(Store.writing_api_key)
+
+
 # TODO добавить проверку наличия пользователя в БД
-@router.message(Person.writing_api_key)
+@router.message(Store.writing_api_key)
 async def store_chosen(message: Message, state: FSMContext):
     await state.update_data(chosen_store=message.text.lower())
     await message.answer(
