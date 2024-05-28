5ab3a00376b25a6c05d5a190ca57810e9bb4ce31
SoniaFilozop <72881848+SoniaFilozop@users.noreply.github.com>
2024-05-28 10:55:44 +0300

    add promos_products, promos_products_activate, promos_products_deactivate  in utils

--- ozon/utils.py
+++ ozon/utils.py
@@ -68,17 +68,92 @@ class Utils:
 
         return all_candidates
 
+    async def promos_products(self, action_id):
+        endpoint = '/v1/actions/products'
+        url = self.base_url + endpoint
+        payload = {
+            "action_id": action_id,
+            "limit": 10,
+            "offset": 0
+        }
+
+        async with aiohttp.ClientSession(headers=self.headers) as session:
+            async with session.post(url, json=payload) as response:
+                if response.status == 200:
+                    all_products = await response.json()
+                else:
+                    print(f"Error: {response.status}")
+                    try:
+                        error_response = await response.json()
+                        print(error_response)
+                    except aiohttp.ContentTypeError:
+                        print("Response is not JSON")
+                    return None
+
+        return all_products
+
+    async def promos_products_activate(self, action_id, products):
+        endpoint = '/v1/actions/products/activate'
+        url = self.base_url + endpoint
+        payload = {
+            "action_id": action_id,
+            "products": products
+        }
+
+        async with aiohttp.ClientSession(headers=self.headers) as session:
+            async with session.post(url, json=payload) as response:
+                if response.status == 200:
+                    result = await response.json()
+                else:
+                    print(f"Error: {response.status}")
+                    try:
+                        error_response = await response.json()
+                        print(error_response)
+                    except aiohttp.ContentTypeError:
+                        print("Response is not JSON")
+                    return None
+
+        return result
+
+    async def promos_products_deactivate(self, action_id, products_ids):
+        endpoint = '/v1/actions/products/deactivate'
+        url = self.base_url + endpoint
+        payload = {
+            "action_id": action_id,
+            "product_ids": products_ids
+        }
+
+        async with aiohttp.ClientSession(headers=self.headers) as session:
+            async with session.post(url, json=payload) as response:
+                if response.status == 200:
+                    result = await response.json()
+                else:
+                    print(f"Error: {response.status}")
+                    try:
+                        error_response = await response.json()
+                        print(error_response)
+                    except aiohttp.ContentTypeError:
+                        print("Response is not JSON")
+                    return None
+
+        return result
+
     async def connection(self):
         promos = await self.promos()
         if promos:
-            print("Promos:")
+            print("Акции:")
             print(promos)
 
         action_id = '1177179'  # Укажите здесь нужный action_id
         candidates = await self.promos_candidates(action_id)
         if candidates:
-            print("Candidates:")
+            print("Кандидаты:")
             print(candidates)
+
+        products = await self.promos_products(action_id)
+        if products:
+            print("Товары в акциях:")
+            print(products)
 
 
 def main():
